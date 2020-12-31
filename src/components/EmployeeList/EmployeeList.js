6f2e57272959393b39d640f10bdc568fb1b0ce74
Simone Cheah <simone_cheah@hotmail.com>
2020-12-31 22:55:53 +1100

    moved map outside of component

--- src/components/EmployeeList/EmployeeList.js
+++ src/components/EmployeeList/EmployeeList.js
@@ -2,18 +2,30 @@ import React from "react";
 // import "./style.css"
 
 const EmployeeList = ({ handleShowModal, results }) => {
-    return <ul>
-        { results.employeeList.map(({ name, id, image, role, gender, dateOfBirth, email }) => (
-            <li key={ id } onClick={ (e) => handleShowModal({ name, image, role, gender, dateOfBirth, email }, e) }>
-                <div>{ name }</div>
-                <div>{ image }</div>
-                <div>{ role }</div>
-                <div>{ gender }</div>
-                <div>{ dateOfBirth }</div>
-                <div>{ email }</div>
-            </li>
-        )) }
-    </ul>;
+    // return <ul>
+    //     { results.employeeList.map(({ name, id, image, role, gender, dateOfBirth, email }) => (
+    //         <li key={ id } onClick={ (e) => handleShowModal({ name, image, role, gender, dateOfBirth, email }, e) }>
+    //             <div>{ name }</div>
+    //             <div>{ image }</div>
+    //             <div>{ role }</div>
+    //             <div>{ gender }</div>
+    //             <div>{ dateOfBirth }</div>
+    //             <div>{ email }</div>
+    //         </li>
+    //     )) }
+    // </ul>;
+    const { name, id, image, role, gender, dateOfBirth, email } = results;
+
+    return <li key={ id } onClick={ (e) => handleShowModal({ name, image, role, gender, dateOfBirth, email }, e) }>
+        {/* <div>{ id }</div> */ }
+        <div>{ name }</div>
+        <div>{ image }</div>
+        <div>{ role }</div>
+        <div>{ gender }</div>
+        <div>{ dateOfBirth }</div>
+        <div>{ email }</div>
+    </li>;
+
 };
 
 export default EmployeeList;
