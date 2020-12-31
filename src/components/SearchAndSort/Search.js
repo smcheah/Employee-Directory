b6e2afce9844fa21feee962f687fe1708402fec6
Simone Cheah <simone_cheah@hotmail.com>
2021-01-01 04:53:14 +1100

    changed div to hold whole component

---
+++ src/components/SearchAndSort/Search.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+const Search = ({ search, onChangeHandler, handleSearch, handleReset }) => {
+    return <div className="search">
+        <label>Enter a search term:</label>
+        <input type="search" name="search" id="searchInput" value={ search } onChange={ onChangeHandler } placeholder="search term" />
+        <button onClick={ handleSearch }>Search</button>
+        <button onClick={ handleReset }>Reset</button>
+    </div>;
+};
+
+export default Search;
