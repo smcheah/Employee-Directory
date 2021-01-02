8384fce0979001cf33258940cef5318e2a4f850c
Simone Cheah <simone_cheah@hotmail.com>
2021-01-03 08:25:22 +1100

    removed link router

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,8 @@
 import React from "react";
-import { Link } from "react-router-dom"; // link to home page, about, mission statement
 
 const Navbar = () => {
     return <nav>
-        <a href="/">HELLO </a>
-        <Link to="/">COMPANY NAME</Link>
+        <a href="/">HELLO COMPANY</a>
     </nav>;
 };
 
