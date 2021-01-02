50c5b4e6bc974b09934dad75296fb69474c3c49f
Simone Cheah <simone_cheah@hotmail.com>
2021-01-03 08:23:59 +1100

    added associated label

--- src/components/SearchAndSort/Search.js
+++ src/components/SearchAndSort/Search.js
@@ -3,7 +3,7 @@ import "./style.css";
 
 const Search = ({ search, onChangeHandler, handleSearch, handleReset }) => {
     return <div className="search">
-        <label>Enter a search term:</label>
+        <label htmlFor="searchInput">Enter a search term:</label>
         <input type="search" name="search" id="searchInput" value={ search } onChange={ onChangeHandler } placeholder="search term" />
         <button onClick={ handleSearch }>Search</button>
         <button onClick={ handleReset }>Reset</button>
