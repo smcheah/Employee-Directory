6efe1fdce01be597264a5ec4059ea1ea0f1ad663
Simone Cheah <simone_cheah@hotmail.com>
2021-01-03 08:26:46 +1100

    removed router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import MainPage from "./components/MainPage";
 import Footer from "./components/Footer";
@@ -9,11 +8,11 @@ import employees from "./data/employees.json";
 // the table of contents
 const App = () => {
 
-    return <Router>
+    return <>
         <Navbar />
         <MainPage { ...{ employees } } />
         <Footer />
-    </Router >;
+    </ >;
 };
 
 export default App;
